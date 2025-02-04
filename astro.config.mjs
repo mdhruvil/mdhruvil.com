23e1c1a881161c9f6760188f4c366ca3b61f2eb8
mdhruvil <dhruvil1808@gmail.com>
2025-02-04 19:51:25 +0530

    feat: add projects page and integrate expressive code support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,18 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mdhruvil.github.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [expressiveCode(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
     shikiConfig: {
-      theme: "dracula-soft",
+      theme: "catppuccin-mocha",
     },
   },
 });
