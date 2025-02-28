b6ac7c80d849990198959382d899815c5a1a56d8
mdhruvil <dhruvil1808@gmail.com>
2025-02-28 13:48:17 +0530

    feat: remove redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,6 @@ import expressiveCode from "astro-expressive-code";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mdhruvil.github.io",
-  redirects: {
-    "/resume":
-      "https://docs.google.com/document/d/1tDJ9bKlYkeekRE0KtiqJWGPXz3gQl5LFuLADH3j4BwU/edit?usp=sharing",
-  },
   integrations: [expressiveCode(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
