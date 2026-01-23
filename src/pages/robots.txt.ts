204bf7f20e155ede145d33086fe7bb0f371c848a
mdhruvil <git@mdhruvil.com>
2026-01-23 12:44:51 +0530

    feat: add form url

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,8 +2,9 @@ export async function GET() {
   return new Response(
     `User-agent: *
 Allow: /
+Disallow: /go
 
 Sitemap: https://mdhruvil.com/sitemap-index.xml
-        `.trim(),
+      `.trim(),
   );
 }
